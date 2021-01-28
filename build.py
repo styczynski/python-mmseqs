d709a3e711599e2161f2a380031655bc5172df9d
Piotr Styczyński <piotrs@radcode.co>
2021-01-28 01:11:13 +0100

    WIP: Try to fix compilation for windows x64

--- build.py
+++ build.py
@@ -58,6 +58,7 @@ class CMakeBuild(build_ext):
             ]
             if sys.maxsize > 2 ** 32:
                 cmake_args += ["-A", "x64"]
+                build_args += ["-A", "x64"]
             build_args += ["--", "/m"]
         else:
             cmake_args += ["-DCMAKE_BUILD_TYPE=" + cfg]
