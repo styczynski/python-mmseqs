0ee85711781bbef829e7e2afa9d1a05f4e7f6445
Piotr Styczyński <piotrs@radcode.co>
2021-01-29 04:13:33 +0100

    WIP: Fix GH Actions Windows build

--- src/mmseqs_native/src/commons/alloc.h
+++ src/mmseqs_native/src/commons/alloc.h
@@ -9,6 +9,8 @@
 #define VLA(TYPE, NAME, SIZE) TYPE* NAME = (TYPE*) malloc((SIZE) * sizeof(TYPE))
 #else
 #if _MSC_VER && !__INTEL_COMPILER
+#include <windows.h>
+#include <malloc.h>
 #define VLA(TYPE, NAME, SIZE) TYPE* NAME = (TYPE*) _malloca((SIZE) * sizeof(TYPE))
 #else
 #define VLA(TYPE, NAME, SIZE) TYPE NAME[(SIZE)]
