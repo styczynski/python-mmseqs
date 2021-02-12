7be7622c268db8f222f35d034d180a1acb4b0e0c
Piotr Styczyński <piotrs@radcode.co>
2021-02-12 19:15:55 +0100

    WIP: Try to fix GH build

--- setup_cibuildwheel.sh
+++ setup_cibuildwheel.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Install Poetry..."
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
+
 echo "Prepare cibuildwheel global dependencies..."
 poetry export -f requirements.txt --output requirements.txt
 pip install -r requirements.txt
