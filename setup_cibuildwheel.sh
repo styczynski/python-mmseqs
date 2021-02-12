9c12bf2c508c1009b7792c2ed3991290921dfcb2
Piotr Styczyński <piotrs@radcode.co>
2021-02-12 19:33:24 +0100

    WIP: Try to fix GH build

--- setup_cibuildwheel.sh
+++ setup_cibuildwheel.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "Check if all packages are present..."
-apt-get update
-apt-get install -y python3 python3-setuptools python3-pkg-resources python3-pip python3-dev libffi-dev build-essential git
-
 echo "Install Poetry..."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
 export PATH="$PATH:$HOME/.poetry/bin"
