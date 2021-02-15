25741dde765844ddf62897d03d325d7aa58d3a64
Piotr Styczyński <piotrs@radcode.co>
2021-02-15 17:43:16 +0100

    Fix support for tseq/qseq

--- src/biosnake_native/bindings.cpp
+++ src/biosnake_native/bindings.cpp
@@ -144,6 +144,10 @@ PYBIND11_MODULE(biosnake_native, m) {
                      &biosnake_blast_tab_record::query_sequence_aligned)
       .def_readwrite("target_sequence_aligned",
                      &biosnake_blast_tab_record::target_sequence_aligned)
+      .def_readwrite("target_sequence_content",
+                     &biosnake_blast_tab_record::target_sequence_content)
+      .def_readwrite("query_sequence_content",
+                     &biosnake_blast_tab_record::query_sequence_content)
       .def_readwrite("e_value", &biosnake_blast_tab_record::e_value)
       .def_readwrite("bit_score", &biosnake_blast_tab_record::bit_score)
       .def("__repr__", &biosnake_blast_tab_record::toString);
